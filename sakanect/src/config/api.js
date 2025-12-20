7d3e46a96bec4d7abcd0a16bb8897089fcbc7036
xavryl <johncilbert312@gmail.com>
2025-12-20 18:06:21 +0800

    test3

--- sakanect/src/config/api.js
+++ sakanect/src/config/api.js
@@ -4,4 +4,4 @@ const isLocal = window.location.hostname === 'localhost' || window.location.host
 // If local, use localhost. If public (DevTunnel), use your specific Tunnel URL.
 export const API_URL = isLocal 
   ? 'http://localhost:5000' 
-  : 'https://k3p5770c-5000.asse.devtunnels.ms';
+  : 'https://capstone-0h24.onrender.com';
